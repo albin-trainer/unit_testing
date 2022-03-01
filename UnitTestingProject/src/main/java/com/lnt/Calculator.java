02247b516e1d99712eabeea65c9cc14f54dd4c0c
albin-trainer <albinxavier@gmail.com>
2022-03-01 17:49:58 +0530

    a a a a a

---
+++ UnitTestingProject/src/main/java/com/lnt/Calculator.java
@@ -0,0 +1,7 @@
+package com.lnt;
+
+public class Calculator {
+int add(int a,int b) {
+	return a+b;
+}
+}
---
+++ UnitTestingProject/src/test/java/com/lnt/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.lnt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void testAdd() {
+		Calculator c= new Calculator();
+		assertEquals(10, c.add(5, 5));
+	}
+
+}
